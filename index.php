bb253ad5ba2d866a7ac2f36918b68393b0f53267
saxsir <saxsir.256@gmail.com>
2015-03-25 15:08:42 +0900

    [add] close #1 add sample files :beer:

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+    <link href="main.css" rel="stylesheet">
+    <script src="app.js"></script>
+  </head>
+  <body>
+    <?php echo '<p>Hello World</p>'; ?>
+  </body>
+</html>
